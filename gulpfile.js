cf82bf170967e82d0eed573298a64e6edabfed94
michelml <michmoreau.l@gmail.com>
2022-04-28 18:08:40 -0400

    build package entirely with gulp for cross-shell/os compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
-const { src, dest } = require("gulp");
+const { execSync } = require("child_process");
+const gulp = require("gulp");
 
 function rdkitCopy(cb) {
-  return src(
-    "node_modules/@rdkit/rdkit/Code/MinimalLib/dist/RDKit_minimal.*"
-  ).pipe(dest("public/"));
+  return gulp
+    .src("node_modules/@rdkit/rdkit/Code/MinimalLib/dist/RDKit_minimal.*")
+    .pipe(gulp.dest("public/"));
+}
+
+function packageProject(cb) {
+  execSync("pika-pack", { stdio: "inherit" });
+  cb();
 }
 
 exports.rdkitCopy = rdkitCopy;
+exports.packageProject = packageProject;
+exports.buildPackage = gulp.series(rdkitCopy, packageProject);
